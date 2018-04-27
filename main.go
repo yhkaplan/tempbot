c94e5b9303a560987e388711c8e699d4b5c8c8a6
yhkaplan <yhkaplan@gmail.com>
2018-04-27 22:48:00 +0900

    Added degree sign and removed space before percentage sign

--- main.go
+++ main.go
@@ -67,7 +67,7 @@ func respond(rtm *slack.RTM, msg *slack.MessageEvent, prefix string) {
 		fmt.Println(err)
 		msgTxt = fmt.Sprintf("Error occurred: %s", err)
 	} else {
-		msgTxt = fmt.Sprintf("Temperature: %vC, Humidity: %v %%", temp, humid) //TODO: Add degree sign
+		msgTxt = fmt.Sprintf("Temperature: %v\u00B0C, Humidity: %v%%", temp, humid)
 	}
 
 	rtm.SendMessage(rtm.NewOutgoingMessage(msgTxt, msg.Channel))
